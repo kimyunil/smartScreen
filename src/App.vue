9c3766ec7f7d30fb50456a7d6c733dcba9cec7c0
Chang Long Zhu <chlzr@hotmail.com>
2018-05-07 18:19:08 -0700

    Adding a key event to skip OOBE, they key is PLAY/PAUSE

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,8 @@ export default {
   mounted() {
     globalListener.init();
     Messages.$on('smartscreen.setup', this.setupScreen);
-    Messages.send('smartscreen.setupscreen', { from: 'smartscree'});
+    Messages.$on('button_down', this.handleButton);
+    Messages.send('smartscreen.setupscreen', { from: 'smartscreen' });
     if (window.sessionStorage.getItem('oobe') !== null) {
       this.setUpcomplete(true);
     }
@@ -36,6 +37,7 @@ export default {
   },
   data() {
     return {
+      setupInProcess: true,
       page: 0,
       localsetup: false,
       screens: ['/static/Images/tv setup 01.png', '/static/Images/tv setup 02.png'],
@@ -55,14 +57,26 @@ export default {
       console.log(param);
       if (param.page === 0) {
         this.page = 0;
+        this.setupInProcess = true;
       } else if (param.page === 9) {
         window.sessionStorage.setItem('oobe', 'true');
         // document.cookie = "oobe=true";
         this.switch_comp({ replace: true, name: 'home' });
         this.setUpcomplete(true);
         this.page = 0;
+        this.setupInProcess = true;
       } else {
         this.page = 1;
+        this.setupInProcess = true;
+      }
+    },
+    handleButton(param) {
+      console.log('param key: ', param);
+      if (param === 'PLAY' && this.setupInProcess) {
+        this.setupInProcess = false;
+        this.switch_comp({ replace: true, name: 'home' });
+        this.setUpcomplete(true);
+        this.page = 0;
       }
     },
   },
